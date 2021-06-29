b7240b31dd203cf28404332b042bcc0a335f2fae
Claire <clairelogan16@gmail.com>
2021-06-29 10:10:47 -0700

    commit tree 22b12c39de0fe1b22634d2c20d12c6fdc22f7c64

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function receivesAFunction(callBack) {
-    callBack();
+// function receivesAFunction(callBack) {
+//     callBack();
+// }
+
+// function returnsANamedFunction() {
+//     return function sampleFunction() {
+//         console.log('Test function');
+//     }
+// }
+
+// function returnsAnAnonymousFunction() {
+//     return () => console.log('Anonymous function');
+// }
+
+function receivesAFunction(callback){
+    return callback();
 }
 
 function returnsANamedFunction() {
-    return function sampleFunction() {
-        console.log('Test function');
+    return function namedFunction(){
+        return 'Sample function';
     }
 }
 
-function returnsAnAnonymousFunction() {
-    return () => console.log('Anonymous function');
+function returnsAnAnonymousFunction(){
+    return function(){return 'Sample function';}
 }
